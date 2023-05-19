4d2cdcfbc4375aa34f35107387f2f6ad0840adf2
N.N.L.Keerthana <nyshadham21bcs185@iiitkottayam.ac.in>
2023-05-19 17:09:02 +0530

    edit 1

--- Flutter/mur/lib/components/constants.dart
+++ Flutter/mur/lib/components/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const String google_api_key = "API_Key";
+const String google_api_key = "AIzaSyChyanawTPrdUWJfY9Z1lU9dmyzXnQALU8";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
